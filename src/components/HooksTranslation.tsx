9682a454ecee25f2334e186de08efe21fafe065c
bnayae <Bnayanet@hotmail.com>
2019-07-23 16:50:53 +0300

    escape

--- src/components/HooksTranslation.tsx
+++ src/components/HooksTranslation.tsx
@@ -13,11 +13,10 @@ export function HooksTranslation() {
     return (
         <>
             <h1>Hooks</h1>
-            <h3>{t('title', { item: "React i18n" })}</h3>
-            <h3>{t('hi', { name: "bnaya" })}</h3>
+            <p dangerouslySetInnerHTML={{ __html: t('title', { item: "React i18n" }) }} />
+            <p dangerouslySetInnerHTML={{ __html: t('hi', { name: "<b>bnaya</b>" }) }} />
             <h4>{t('description.part1')}</h4>
             <h5>{t('description.part2')}</h5>
-            <hr />
         </>
     );
 }
